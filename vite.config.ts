1360dc533123348d2512588967bc40fd92b84de9
h4riharan <hariharanmh001@gmail.com>
2025-10-13 02:57:10 +0530

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.ts', // or src/index.js
+      name: 'GifWorldReact',
+      fileName: 'index',
+      formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      // Make sure to externalize peer dependencies (like react)
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
       },
-    }),
-  ],
-})
+    },
+  },
+});
